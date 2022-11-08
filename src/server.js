9204c5822e3b398b19f02e8f47ceaffd712f3fc3
eden <gmean7@gmail.com>
2022-11-08 17:20:41 +0900

    url logger 추가

--- src/server.js
+++ src/server.js
@@ -4,9 +4,20 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Here is home");
-});
+// middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url} 이동중입니다.`);
+  next();
+};
+
+// controller
+const handleHome = (req, res) => {
+  return res.send("Here is MAIN HOMEPAGE");
+};
+
+// route
+app.use(logger);
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listeting on port ${PORT}`);
