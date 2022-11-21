8806b1b7e9624c36b0c829fe7fbdfa0ce00689a4
eden <gmean7@gmail.com>
2022-11-21 17:48:04 +0900

    라우터 1차 적용 및 정상 작동 확인

--- src/server.js
+++ src/server.js
@@ -6,16 +6,24 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-// middleware
+app.use(logger);
+
+// routers
+const globalRouter = express.Router();
+const artworksRouter = express.Router();
 
 // controller
-const handleHome = (req, res) => {
-  return res.send("Here is MAIN HOMEPAGE");
-};
+const handleHome = (req, res) => res.send("Home");
+const handleArtwoks = (req, res) => res.send("Our All artworks.");
+const handleToyArtworks = (req, res) => res.send("Here is art toys.");
+
+globalRouter.get("/", handleHome);
+artworksRouter.get("/", handleArtwoks);
+artworksRouter.get("/toy", handleToyArtworks);
 
 // route
-app.use(logger);
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/artworks", artworksRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listeting on port ${PORT}`);
