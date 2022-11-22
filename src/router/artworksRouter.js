7788bf43a06c501ad54d6da866ae8827f4e45fc0
JIMIN PARK <bat_zimin@Jiminui-MacBookPro.local>
2022-11-22 18:35:39 +0900

    아트워크 라우터 파일 내부에 있는 컨트롤러 분리하여 파일 생성 | 임포트 확인 완료

--- src/router/artworksRouter.js
+++ src/router/artworksRouter.js
@@ -1,15 +1,14 @@
 import express from "express";
+import {
+  handleFrame,
+  handleToy,
+  handleObject,
+  handleTextile,
+  handleHomeware,
+} from "../controller/artworksController";
 
 const artworksRouter = express.Router();
 
-// const handleArtwoks = (req, res) => res.send("Our All artworks.");
-const handleToy = (req, res) => res.send("Here is art toys.");
-const handleTextile = (req, res) => res.send("Here is textile");
-const handleObject = (req, res) => res.send("Here is object");
-const handleFrame = (req, res) => res.send("Here is frame");
-const handleHomeware = (req, res) => res.send("Here is homeware");
-
-// artworksRouter.get("", handleArtwoks);
 artworksRouter.get("/toy", handleToy);
 artworksRouter.get("/textile", handleTextile);
 artworksRouter.get("/object", handleObject);
